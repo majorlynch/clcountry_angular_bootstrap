310810b2dc8c663ddf224b81904690ffafbc07b4
Conor/Lynch <conor.lynch@legato.com>
2024-10-30 15:01:24 +0000

    added country model

--- src/app/model/country.model.ts
+++ src/app/model/country.model.ts
@@ -22,3 +22,95 @@ export interface Country {
     timezones: string;
     coatOfArms: string;
 }
+
+export interface CountryFull
+    {
+      name: {
+        common: string,
+        official: string,
+        nativeName: {
+          deu: {
+            official: string,
+            common: string
+          }
+        }
+      },
+      tld: string[],
+      cca2: string,
+      ccn3: string,
+      cca3: string,
+      cioc: string,
+      independent: boolean,
+      status: string,
+      unMember: boolean,
+      currencies: {
+        currencyCode: {
+          name: string,
+          symbol: string
+        }[]
+      },
+      idd: {
+        root: string,
+        suffixes: [
+          suffix:string
+        ]
+      }[],
+      capital: string[
+      ],
+      altSpellings: string[],
+      region: string,
+      subregion: string,
+      languages: {
+        language: string
+      },
+      translations: {
+        translation: {
+          official: string,
+          common: string,
+        }[],
+      },
+      latlng: string[],
+      landlocked: boolean,
+      borders: string[],
+      area: string,
+      demonyms: {
+        demonyn: {
+          f: string,
+          m: string,
+        }[]
+      },
+      flag: string,
+      maps: {
+        googleMaps: string,
+        openStreetMaps: string,
+      },
+      population: number,
+      gini: {
+        year: string
+      },
+      fifa: string,
+      car: {
+        signs: string[],
+        side: string
+      },
+      timezones: string[],
+      continents: string[],
+      flags: {
+        png: string,
+        svg: string,
+        alt: string,
+      },
+      coatOfArms: {
+        png: string,
+        svg: string
+      }[],
+      startOfWeek: string,
+      capitalInfo: {
+        latlng:  number[]
+      },
+      postalCode: {
+        format: string,
+        regex: string
+      }
+    }
+  ;
